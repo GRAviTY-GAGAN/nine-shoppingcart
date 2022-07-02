938405bda77bb81329996799543538aaa8da910d
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-02 11:11:23 +0530

    Updates

---
+++ precache-manifest.ed6d9c655ca4dd102c2c0398bcc78b83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1627112e71e2eab4ddaf23f6a931311",
+    "url": "/nine-shoppingcart/index.html"
+  },
+  {
+    "revision": "9098e1466f6e268c9d43",
+    "url": "/nine-shoppingcart/static/css/2.2e92ac44.chunk.css"
+  },
+  {
+    "revision": "9627ba078d7266f8bdd1",
+    "url": "/nine-shoppingcart/static/css/main.f5b27828.chunk.css"
+  },
+  {
+    "revision": "9098e1466f6e268c9d43",
+    "url": "/nine-shoppingcart/static/js/2.117ad33f.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/nine-shoppingcart/static/js/2.117ad33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9627ba078d7266f8bdd1",
+    "url": "/nine-shoppingcart/static/js/main.889826f9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/nine-shoppingcart/static/js/main.889826f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c917bcf26571b4c16c84",
+    "url": "/nine-shoppingcart/static/js/runtime-main.7ff5abc8.js"
+  }
+]);
